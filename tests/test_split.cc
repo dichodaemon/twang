3e2f2f6bdc99611126b874ae2fde1a1de9ceadbb
Dizan Vasquez <dichodaemon@gmail.com>
2026-09-19 00:39:34 +0000

    test(split): 3.4 thread note through NoteOn/NoteOff

--- tests/test_split.cc
+++ tests/test_split.cc
@@ -26,9 +26,9 @@ int main() {
             control.SetParamDisp(0, ParamRef{0, ParamId::kCutoff}, 100.0f + (i % 10) * 500.0f);
             control.SetParamDisp(0, ParamRef{0, ParamId::kResonance}, (i % 10) * 10.0f);
             const float freq = 220.0f + (i % 12) * 55.0f;
-            control.NoteOn(0, freq, 127);
+            control.NoteOn(0, 60, freq, 127);
             std::this_thread::sleep_for(std::chrono::milliseconds(2));
-            control.NoteOff(0, freq);
+            control.NoteOff(0, 60);
             std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
         done.store(true, std::memory_order_release);
